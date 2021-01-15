7b23c68a01000fb4a96cf497a0cde13c48507bdf
carl0sR0ma0 <cvpromao@gmail.com>
2021-01-15 16:08:38 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/carlosromao/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.carlosromao.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carlosromao.cursomc.domain.Categoria;
+import com.carlosromao.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	
+}
